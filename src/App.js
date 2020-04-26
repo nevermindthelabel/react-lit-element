4567d3466df2b25374c3160ac5415f5661ab76b8
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-25 21:32:16 -0700

    initial import of lit element

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './lit-element/custom-component.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <custom-element>test</custom-element>
     </div>
   );
 }
