b81d8d40495c0b91fdd5e0032ee3593951f80ad8
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-27 21:38:09 -0700

    cleaned up code

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,20 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-// import 'web-component-essentials';
+import React from 'react';
 import './lit-element/custom-component.js';
 
 const Wrapper = (props) => {
-  console.log(props)
-  const ref = useRef();
-
-  useEffect(() => {
-  //   ref.current.title = props.title;
-  //   ref.current.addEventListener('show', e => props.onShow(e));
-  // }, [props])
-  });
-
-  return (
-  // <x-dropdown ref={ref}>{props.chidren}</x-dropdown>
-  <custom-element ref={ref}>{props.children}</custom-element>
+   return (
+  <custom-element>{props.children}</custom-element>
   );
 }
 
